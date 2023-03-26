698129f23d4e45aad974bb929c70d052c01702de
Yevhenii Kolesnichenko <kolesnichenko.yevhenii@pdffiller.team>
2023-03-26 19:25:28 +0300

    example config

---
+++ src/config-example.h
@@ -0,0 +1,21 @@
+#define WIFI_SSID     "**********"
+#define WIFI_PASSWORD "**********"
+
+#define ID "room_comfort" // Add unique name for this sensor
+#define INTERVAL 30 //seconds
+
+#define DHTTYPE DHT11 // Type DHT 11
+
+#define PIN_DHT 13    // Which pin is DHT 11 connected to
+#define PIN_LED 2
+#define PIN_BUTTON 21
+
+// For more information on where to get these values see: https://github.com/grafana/diy-iot/blob/main/README.md#sending-metrics
+#define GC_PROM_URL "prometheus-us-central1.grafana.net"
+#define GC_PROM_PATH "/api/prom/push"
+#define GC_PORT 443
+#define GC_PROM_USER "*****"
+#define GC_PROM_PASS "********************"
+
+#define TG_BOT_TOKEN "***************"
+#define TG_CHAT_ID "**********"
