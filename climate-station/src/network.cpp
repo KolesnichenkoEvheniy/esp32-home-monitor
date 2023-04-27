7a0005e3e6af81c3dd8402f019137d7d633e5f2d
Yevhenii Kolesnichenko <kolesnichenko.yevhenii@pdffiller.team>
2023-04-27 18:47:50 +0300

    output measurements on web server page

--- climate-station/src/network.cpp
+++ climate-station/src/network.cpp
@@ -51,6 +51,14 @@ bool initWiFi() {
 String processor(const String& var) {
   if(var == "INTERVAL") {
     return String(refreshInterval);
+  } else if(var == "TEMPERATURE"){
+    return String(measurements.temperature) + " C";
+  } else if(var == "HUMIDITY"){
+    return String(measurements.humidity) + " %";
+  } else if(var == "CO2"){
+    return String(measurements.eco2) + " ppm";
+  } else if(var == "TVOC"){
+    return String(measurements.tvoc) + "";
   }
   return String();
 }
